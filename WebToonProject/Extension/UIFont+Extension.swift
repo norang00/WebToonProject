ec2577ce9a7eee0dec46a388822136518807b890
norang <norang1221@gmail.com>
2025-03-21 13:46:56 +0900

    [Chore] Delete Unused Fonts

--- WebToonProject/Extension/UIFont+Extension.swift
+++ WebToonProject/Extension/UIFont+Extension.swift
@@ -20,18 +20,6 @@ extension UIFont {
         return UIFont(name: "Pretendard-Bold", size: size) ?? UIFont.systemFont(ofSize: size, weight: .bold)
     }
     
-    static func pretendardJPRegular(ofSize size: CGFloat) -> UIFont {
-        return UIFont(name: "PretendardJP-Regular", size: size) ?? UIFont.systemFont(ofSize: size, weight: .regular)
-    }
-    
-    static func pretendardJPMedium(ofSize size: CGFloat) -> UIFont {
-        return UIFont(name: "PretendardJP-Medium", size: size) ?? UIFont.systemFont(ofSize: size, weight: .medium)
-    }
-    
-    static func pretendardJPBold(ofSize size: CGFloat) -> UIFont {
-        return UIFont(name: "PretendardJP-Bold", size: size) ?? UIFont.systemFont(ofSize: size, weight: .bold)
-    }
-    
     static var titleFont: UIFont {
         return UIFont.pretendardBold(ofSize: 20)
     }
@@ -44,15 +32,4 @@ extension UIFont {
         return UIFont.pretendardMedium(ofSize: 12)
     }
     
-    static var jpTitleFont: UIFont {
-        return UIFont.pretendardJPBold(ofSize: 20)
-    }
-    
-    static var jpBodyFont: UIFont {
-        return UIFont.pretendardJPRegular(ofSize: 16)
-    }
-    
-    static var jpCaptionFont: UIFont {
-        return UIFont.pretendardJPMedium(ofSize: 12)
-    }
 }
